bbff46ca53a2f4ad3fa7431d879530697580a5de
Gannan02 <145112714+Gannan02@users.noreply.github.com>
2024-10-27 16:52:21 +0800

    Update SC_A.sol

--- SC_A.sol
+++ SC_A.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ChallengeGenerator {
+contract SC_A {
     event Deposit(uint[] si, uint[] vi);
 
     function ChallGen(uint c) public returns (uint[] memory, uint[] memory) {

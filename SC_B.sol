d1f632f0ab655571649c22f8f4abe32462ea9965
Gannan02 <145112714+Gannan02@users.noreply.github.com>
2024-11-06 14:38:11 +0800

    Update SC_B.sol

--- SC_B.sol
+++ SC_B.sol
@@ -5,7 +5,7 @@ contract SC_B {
     event Deposit(bytes32 indexed TrSC_A, bytes32 indexed Trproof, bytes32 S, uint256 timestamp);
     event NotifyRecipient(address recipient, bytes32 psi);
 
-    function AuditConsolidation(bytes32 TrSC_A, bytes32 Trproof, bytes32 S1, bytes32 S2, bytes32 psi) public {
+    function AuditProcessPreservation(bytes32 TrSC_A, bytes32 Trproof, bytes32 S1, bytes32 S2, bytes32 psi) public {
         bytes32 S = keccak256(abi.encodePacked(S1, S2, psi));
 
         emit Deposit(TrSC_A, Trproof, S, block.timestamp);
